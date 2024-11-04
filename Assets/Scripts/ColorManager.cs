4fddcbcb2d3f3e9af987a537e657fa5cecf0714b
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-04 13:25:05 -0600

    Created ColorManager

---
+++ Assets/Scripts/ColorManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ColorManager is a class that controlls the colors
+/// of elements on the screen. It tells objects to update
+/// their color.
+/// </summary>
+public class ColorManager : KeywordHandler
+{
+    public override string ReadTokens(string[] tokens)
+    {
+        return "Tokens Read";
+    }
+}
