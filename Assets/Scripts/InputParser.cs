f34a523455346ea48f630ae3cb059c6f4af1e985
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-04 13:15:31 -0600

    Finally got the InputParser to start reading inputs

--- Assets/Scripts/InputParser.cs
+++ Assets/Scripts/InputParser.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 /// <summary>
 /// InputParser is an object to be used with text input.
-/// It takes a string as an input, updates the state
-/// of either the scene or the player, and returns either
-/// a result string or a sceen depending on the input.
+/// It takes a string as an input, find the correct input 
+/// handler, and then updates the state of the game.
 /// </summary>
 public class InputParser : MonoBehaviour
 {
+    [Header("Input Field")]
+    [SerializeField] TMP_InputField inputField;
+
+    private void Start()
+    {
+        inputField.onEndEdit.AddListener(text =>
+        {
+            if(Input.GetKeyDown(KeyCode.Return))
+                ParseInput(text);
+        });
+    }
+
+    public int ParseInput(string input)
+    {
+        Debug.Log(input);
+        return 200;
+    }
 }
