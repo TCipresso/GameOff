8cbb2ad9a233e8c3062a2a3e925eeec15b180315
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-04 13:32:33 -0600

    Created ColorStore

---
+++ Assets/Scripts/ColorStore.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ColorTypes { HUDColor, NarratorColor }
+
+/// <summary>
+/// ColorStore is a scriptatble object that stores 
+/// the colors for <see cref="Colorable"/> objects.
+/// </summary>
+public class ColorStore : ScriptableObject
+{
+    [Header("Colors")]
+    [SerializeField] Color narratorColor;
+    [SerializeField] Color hudColor;
+    List<Colorable> colorableObjects = new List<Colorable>();
+
+    public Color GetColor(Colorable obj, ColorTypes wanted)
+    {
+        colorableObjects.Add(obj);
+        switch (wanted)
+        {
+            default:
+            case ColorTypes.HUDColor:
+                return hudColor;
+            case ColorTypes.NarratorColor:
+                return narratorColor;
+        }
+    }
+
+    public void UpdateColors()
+    {
+        //TODO: Go through each colorable and tell them to change.
+    }
+}
