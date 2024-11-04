597b4a9fb9c4262a751d03fd0e7952f7d21b8643
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-04 12:25:10 -0600

    Created InputParser.cs

---
+++ Assets/Scripts/InputParser.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// InputParser is an object to be used with text input.
+/// It takes a string as an input, updates the state
+/// of either the scene or the player, and returns either
+/// a result string or a sceen depending on the input.
+/// </summary>
+public class InputParser : MonoBehaviour
+{
+}
