34661ab13ae85a1e620ddcc98e0d0b53abad609e
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-05 13:14:06 -0600

    Implemented TextOutput

--- Assets/Scripts/TextOutput.cs
+++ Assets/Scripts/TextOutput.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
+public enum OutputCarrot { NONE, USER, SYSTEM, QUESTION }
 
 public class TextOutput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static TextOutput instance { get; private set; }
+    [SerializeField] GameObject textPrefab;
+
+    private void Awake()
     {
-        
+        if (instance != null)
+            Destroy(gameObject);
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Print(string text, ColorTypes colorType=ColorTypes.HUDColor, OutputCarrot outputCarrot=OutputCarrot.SYSTEM)
     {
+        GameObject textObject = Instantiate(textPrefab, transform);
         
+        string outputString;
+        switch(outputCarrot)
+        {
+            case OutputCarrot.NONE:
+                outputString = "";
+                break;
+            case OutputCarrot.USER:
+                outputString = "/>";
+                break;
+            default: 
+            case OutputCarrot.SYSTEM:
+                outputString = ">";
+                break;
+            case OutputCarrot.QUESTION:
+                outputString = "?>";
+                break;
+        }
+        outputString += text;
+        
+        textObject.GetComponent<TextMeshProUGUI>().text = outputString;
+        textObject.GetComponent<Colorable>().colorType = colorType;
     }
 }
