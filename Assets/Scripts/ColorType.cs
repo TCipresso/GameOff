a2d6b0d31296fcc5f69a28d7812f92463113ce77
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-05 13:22:44 -0600

    Added documentation to ColorType

--- Assets/Scripts/ColorType.cs
+++ Assets/Scripts/ColorType.cs
@@ -1,7 +1,15 @@
 /// <summary>
 /// Denotes which type of color your want from <see cref="ColorStore"/>
 /// </summary>
-public enum ColorType { 
-}
+public enum ColorType
+{
+    /// <summary>
+    /// Color of the general HUD elements.
+    /// </summary>
     HUDCOLOR,
+
+    /// <summary>
+    /// Color of dialogue from the narrator color.
+    /// </summary>
     NARRATORCOLOR
+}
