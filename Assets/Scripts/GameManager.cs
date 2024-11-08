a0047751a9d7c7c9f1cb143de11068b7543b7c6a
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-08 14:21:51 -0600

    Documented GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,26 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// GameManager is the class that stores the current
+/// state of the game.
+/// </summary>
 public class GameManager : MonoBehaviour
 {
+    [Header("Starting POI")]
     [SerializeField] PointOfInterest currentPOI;
     public static GameManager instance { private set; get; }
+    
+    /// <summary>
+    /// Singleton
+    /// </summary>
     private void Awake()
     {
         if (instance != null) Destroy(gameObject);
         instance = this;
     }
 
+    /// <summary>
+    /// Gets current POI description.
+    /// </summary>
+    /// <returns></returns>
     public string GetCurrentPOIDesc()
     {
         return currentPOI.GetDescription();
     }
 
+    /// <summary>
+    /// Gets current POI image.
+    /// </summary>
+    /// <returns></returns>
     public Sprite GetCurrentPOIImage()
     {
         return currentPOI.GetImage();
     }
 
+    /// <summary>
+    /// Attempts to move in the direction stated in the tokens.
+    /// </summary>
+    /// <param name="tokens">Tokens from the Move command.</param>
+    /// <returns>The description of the new POI or a line stating move failure.</returns>
     public string AttemptMove(string[] tokens)
     {
         PointOfInterest destination = currentPOI.Move(tokens);
