4a826b557eca039bc05d7e11422dc6ad4768f4cd
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-13 12:31:52 -0600

    Implemented basic getters for encounter

--- Assets/Scripts/Point Of Interest/Encounter.cs
+++ Assets/Scripts/Point Of Interest/Encounter.cs
@@ -5,5 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Scriptable Objects/Encounter")]
 public class Encounter : ScriptableObject
 {
-    
+    [TextArea(3, 10)]
+    [SerializeField] string description = "No Description";
+    [SerializeField] Sprite subjectSprite;
+
+    public string GetDescription()
+    {
+        return description;
+    }
+
+    public Sprite GetSubjectSprite()
+    {
+        return subjectSprite;
+    }
 }
