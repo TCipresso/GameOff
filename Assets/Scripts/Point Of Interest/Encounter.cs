491f44216cddcb486972101f7de9ed0c46c52de7
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-13 14:24:14 -0600

    Documented Encounter

--- Assets/Scripts/Point Of Interest/Encounter.cs
+++ Assets/Scripts/Point Of Interest/Encounter.cs
@@ -2,28 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Scriptable Objects/Encounter")]
+/// <summary>
+/// An Encounter is an activity that is stored in a <see cref="PointOfInterest"/>.
+/// </summary>
+/*[CreateAssetMenu(menuName = "Scriptable Objects/Encounters/Test Encounter")] DO NOT USE THIS ENCOUNTER FOR ANYTHING OTHER THAN TESTS
+ * ALL EXTENSIONS OF ENCOUNTER SHOULD HAVE menuName = "Scriptable Objects/Encounters/abc FOR ITS CreateAssetMenu"*/
 public class Encounter : ScriptableObject
 {
     [TextArea(3, 10)]
-    [SerializeField] string description = "This is a test encounter. Type continue to continue.";
+    [SerializeField] string description = "This is a test encounter. Type \"continue\" to continue.";
     [SerializeField] Sprite subjectSprite;
 
+    /// <summary>
+    /// Get the description of the encounter.
+    /// </summary>
+    /// <returns>The description of the encounter.</returns>
     public string GetDescription()
     {
         return description;
     }
 
+    /// <summary>
+    /// Get the sprite that represents the encounter's subject/character.
+    /// </summary>
+    /// <returns>A <see cref="Sprite"/> of the encounter's subject/character.</returns>
     public Sprite GetSubjectSprite()
     {
         return subjectSprite;
     }
 
+    /// <summary>
+    /// Clean/Update game state and leave the encounter.
+    /// </summary>
     public void LeaveEncounter()
     {
         GameManager.instance.LeaveEnounter();
     }
 
+    /// <summary>
+    /// Checks if encounter can handle player's input.
+    /// </summary>
+    /// <param name="tokens">Tokens from player input.</param>
+    /// <returns>True if encounter can handle input, false otherwise.</returns>
     public bool IsEncounterKeyword(string[] tokens)
     {
         foreach(string token in tokens)
@@ -33,6 +53,11 @@ public class Encounter : ScriptableObject
         return false;
     }
 
+    /// <summary>
+    /// Handles player input.
+    /// </summary>
+    /// <param name="tokens">Tokens from player input.</param>
+    /// <returns>A response message from activity.</returns>
     public string ParseEncounterKeywords(string[] tokens)
     {
         foreach(string token in tokens)
