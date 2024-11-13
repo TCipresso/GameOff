8446678be86050684e8fde8b61524338aae097ea
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-13 12:16:28 -0600

    Created Encounter.cs

---
+++ Assets/Scripts/Point Of Interest/Encounter.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Encounter")]
+public class Encounter : ScriptableObject
+{
+    
+}
