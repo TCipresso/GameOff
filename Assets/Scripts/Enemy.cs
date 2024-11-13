fe94b9343288312dc3104c860132b976176d590c
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-13 12:03:41 -0600

    Created Enemy abstract class

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy
+{
+    [SerializeField] float health;
+    [SerializeField] float damage;
+    [SerializeField] float speed;
+}
