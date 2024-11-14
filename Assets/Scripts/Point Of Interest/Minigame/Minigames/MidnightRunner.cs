a121b6bb9f5100c15d8390ab09b6f757eed4e6ba
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-14 15:12:09 -0600

    MidnightRunner now has end conditions

--- Assets/Scripts/Point Of Interest/Minigame/Minigames/MidnightRunner.cs
+++ Assets/Scripts/Point Of Interest/Minigame/Minigames/MidnightRunner.cs
@@ -6,11 +6,30 @@ public class MidnightRunner : Minigame
 {
     [SerializeField] ObstacleSpawner spawner;
     [SerializeField] GameObject runner;
+    [SerializeField] float gameDuration;
+
     public override void StartMinigame(MinigameCaller caller)
     {
         base.StartMinigame(caller);
+        runner.transform.localPosition = new Vector2(0, -250); //I did not want to mess with localPosition and position. This is going to bite someone in the ass.
         runner.SetActive(true);
+        spawner.reporter = this;
         spawner.SpawnObstacle();
+        StartCoroutine(Timer());
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(gameDuration);
+        EndMinigame();
+        caller.CompleteMinigame(MinigameStatus.WIN);
+    }
+
+    public override void ReportHit()
+    {
+        StopAllCoroutines();
+        EndMinigame();
+        caller.CompleteMinigame(MinigameStatus.LOST);
     }
 
     public override void EndMinigame()
