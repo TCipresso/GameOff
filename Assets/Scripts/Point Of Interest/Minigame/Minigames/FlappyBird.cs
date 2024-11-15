93a5947941bafff58dd7a59744b0da110e9fbed6
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-15 14:57:52 -0600

    Started on FlappyBird

---
+++ Assets/Scripts/Point Of Interest/Minigame/Minigames/FlappyBird.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyBird : Minigame
+{
+    [SerializeField] float duration = 5f;
+    [SerializeField] GameObject bird;
+    [SerializeField] ObstacleSpawner spawner;
+
+    public override void StartMinigame(MinigameCaller caller)
+    {
+        base.StartMinigame(caller);
+        StopAllCoroutines();
+        bird.transform.localPosition = Vector3.zero;
+        bird.SetActive(true);
+        if(bird.TryGetComponent<MoveableObject>(out MoveableObject moveableBird))
+            MovementInput.instance.SetPuppet(moveableBird);
+        else
+            Debug.Log($"{name}: Bird does not have a moveableObject componenet");
+
+        spawner.reporter = this;
+        spawner.SpawnObstacle();
+        StartCoroutine(Timer());
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(duration);
+    }
+
+    public override void EndMinigame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ReportObstacleHit()
+    {
+        throw new System.NotImplementedException();
+    }
+}
