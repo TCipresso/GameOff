81d3e19c5b6d9086324e7b4ea82f95d7ec1a63fd
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-15 14:57:30 -0600

    Created JumpableObject.cs

---
+++ Assets/Scripts/Point Of Interest/Minigame/JumpableObject.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpableObject : MoveableObject
+{
+    [SerializeField] float jumpForce;
+    private bool canJump = true;
+
+    public override void Move(Vector2 input)
+    {
+        /* I don't know what's wrong with this part!
+         * I went back and analyzed my code in a different game and reimplemented it 
+         * here, but it's not working the same. Any horizontal input stops gravity, so
+         * I'm turning horizontal input off for now.
+         */
+        /*Vector3 direction = transform.right * input.x;
+        if (direction.magnitude >= 0.01)
+        {
+            rb.MovePosition(transform.position + direction * Time.fixedDeltaTime * speed);
+        }*/
+
+        if (input.y < 0.01) canJump = true;
+        if (canJump && input.y == 1)
+        {
+            canJump = false;
+            rb.velocity = new Vector2(rb.velocity.x, 0); //You probably shouldn't directly mess with the velocity but it works.
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
