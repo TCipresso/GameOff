04a2ffd2a3a1bb1185b0b4fdec4b5dddcda03d8a
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-27 15:15:31 -0600

    Found incorrect LeaveEncounter in FountainEncounter

--- Assets/Scripts/Point Of Interest/Encounters/FountainEncounter.cs
+++ Assets/Scripts/Point Of Interest/Encounters/FountainEncounter.cs
@@ -63,7 +63,7 @@ public class FountainEncounter : Encounter
     private string AcceptFountain()
     {
         PlayerStats.instance.Heal();
-        GameManager.instance.LeaveEnounter();
+        LeaveEncounter();
         return batheString;
     }
 
@@ -73,7 +73,7 @@ public class FountainEncounter : Encounter
     /// <returns>Description of the player leaving.</returns>
     private string DenyFountain()
     {
-        GameManager.instance.LeaveEnounter();
+        LeaveEncounter();
         return ignoreString;
     }
 }
