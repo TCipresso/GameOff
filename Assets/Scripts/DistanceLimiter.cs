57cb5ae5116443b940e90d9e3c1f7f04a0b7d49a
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-30 16:50:36 -0600

    Fixed Midnight Runner

--- Assets/Scripts/DistanceLimiter.cs
+++ Assets/Scripts/DistanceLimiter.cs
@@ -12,12 +12,19 @@ public class DistanceLimiter : MonoBehaviour
     [SerializeField] bool limitHorizontal;
     [SerializeField] bool limitVertical;
     [SerializeField] bool limitLinear;
+    Vector3 startPoint;
+
+    private void OnEnable()
+    {
+        startPoint = transform.position;
+    }
 
     private void Update()
     {
+        //Debug.Log($"{name}: {Mathf.Abs(transform.position.y - startPoint.y)}");
         if (!limitLinear && !limitHorizontal && !limitVertical) Debug.LogError($"{name} No limit setting set!");
-        else if (limitLinear && Mathf.Abs((transform.position - anchor.position).magnitude) >= distance) gameObject.SetActive(false);
-        else if (limitVertical && Mathf.Abs(transform.position.y - anchor.position.y) >= distance) gameObject.SetActive(false);
-        else if (limitHorizontal && Mathf.Abs(transform.position.x - anchor.position.x) >= distance) gameObject.SetActive(false);
+        else if (limitLinear && Mathf.Abs((transform.position - startPoint).magnitude) >= distance) gameObject.SetActive(false);
+        else if (limitVertical && Mathf.Abs(transform.position.y - startPoint.y) >= distance) gameObject.SetActive(false);
+        else if (limitHorizontal && Mathf.Abs(transform.position.x - startPoint.x) >= distance) gameObject.SetActive(false);
     }
 }
