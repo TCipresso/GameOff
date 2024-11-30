65276d32e09df190b6dad640da65aa83023f82f0
Hans Landgrebe <hlxbl20@gmail.com>
2024-11-30 15:46:52 -0600

    Fixed horse not disappearing after leaving encounter

--- Assets/Scripts/Point Of Interest/Encounters/HorseEncounter.cs
+++ Assets/Scripts/Point Of Interest/Encounters/HorseEncounter.cs
@@ -166,7 +166,7 @@ public class HorseEncounter : Encounter
         PlayerStats.instance.Heal(healthBoostAmount);
         PlayerStats.instance.AddDamage(damageBoostAmount);
         PlayerStats.instance.IncreaseSpeed(speedBoostAmount);
-        GameManager.instance.LeaveEnounter();
+        LeaveEncounter();
         return goodHorseAccept;
     }
 
@@ -176,7 +176,7 @@ public class HorseEncounter : Encounter
     /// <returns>Horse's respone to denial.</returns>
     private string DenyHorse()
     {
-        GameManager.instance.LeaveEnounter();
+        LeaveEncounter();
         if (isEvil)
         {
             return evilHorseDeny;
